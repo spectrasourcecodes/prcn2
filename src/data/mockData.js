6782fee1098c42ce3126cb906573b27aaff27430
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-08-28 01:40:38 +0100

    update support links

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,7 +1,7 @@
 export const SITE_NAME = "Procoin";
 export const ADMIN_EMAIL = "support@procoin.com";
 export const ADMIN_WHATSAPP = "+856484639";
-export const ADMIN_TELEGRAM = "https://t.me/cryptoinvestpro";
+export const ADMIN_TELEGRAM = "https://t.me/";
 
 export const mockUser = {
   id: "USR8781",
