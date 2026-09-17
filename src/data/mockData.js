a80d19b07baaaaac6e2113e97aa051e5fcf7a2ac
Yahaya Abdulbari <yahayaabdulbari13@gmail.com>
2026-09-17 14:26:27 +0100

    Update mockData.js

--- src/data/mockData.js
+++ src/data/mockData.js
@@ -1,6 +1,6 @@
 export const SITE_NAME = "Procoin";
 export const ADMIN_EMAIL = "support@procoin.com";
-export const ADMIN_WHATSAPP = "+856484639";
+export const ADMIN_WHATSAPP = "#";
 export const ADMIN_TELEGRAM = "https://t.me/";
 
 export const mockUser = {
